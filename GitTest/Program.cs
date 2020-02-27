8155212ef960901c9ed40bd50db0a834c2a69245
Andrej-Csharp <61560926+Andrej-Csharp@users.noreply.github.com>
2020-02-27 23:32:02 +0200

    Some work added

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -6,6 +6,8 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
+            //MY work on master branch!
+
             Console.WriteLine("Hello John!! Its me, Luidgy!");
             var userInput = new UserInput();
             userInput.WaitForInput();
