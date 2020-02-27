9e9c65a4b8b87cce3a40aad108eb0f3750dd6fb5
Andrej-Csharp <61560926+Andrej-Csharp@users.noreply.github.com>
2020-02-27 23:10:32 +0200

    Name changed to Rostik

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -6,7 +6,7 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HENLO ANDREJ! Its me, MARIO!");
+            Console.WriteLine("HENLO Rostik!! Its me, MARIO!");
             var userInput = new UserInput();
             userInput.WaitForInput();
         }
